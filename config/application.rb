ba2f428853db8ef000af67332cd3a065bb57660b
Aleksey Bukin <bukin242@yandex.ru>
2020-07-19 05:55:39 +0500

    add translation

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module Indication
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = [:ru, :en]
+    config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
   end
 end
