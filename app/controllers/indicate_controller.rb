a710be0106af52e0f60842b50d3c93255b96ba11
Aleksey Bukin <bukin242@yandex.ru>
2020-07-19 23:44:58 +0500

    validation hot_water and cold water

--- app/controllers/indicate_controller.rb
+++ app/controllers/indicate_controller.rb
@@ -8,12 +8,12 @@ class IndicateController < ApplicationController
     if cold_water.present? || hot_water.present?
       if cold_water.present?
         @cold_water = ColdWater.create(user_id: user.id, value: cold_water)
-        render :new if @cold_water.invalid?
+        render :new and return if @cold_water.invalid?
       end
 
       if hot_water.present?
-        @hot_water = HotWater.create(user_id: user.id, value: cold_water)
-        render :new if @hot_water.invalid?
+        @hot_water = HotWater.create(user_id: user.id, value: hot_water)
+        render :new and return if @hot_water.invalid?
       end
     else
       flash[:alert] = 'Введите значение'
--- app/models/hot_water.rb
+++ app/models/hot_water.rb
@@ -1,3 +1,20 @@
 class HotWater < ApplicationRecord
-  validates :value, numericality: { only_integer: true }
+  validates :value, presence: true, numericality: { only_integer: true }
+  validate :validate_value_month, on: :create
+
+  private
+
+  def validate_value_month
+    year = DateTime.current.year
+    month = DateTime.current.month
+
+    sql = <<-SQL
+      user_id = #{user_id} and
+      date_part('year', created_at) = #{year} and
+      date_part('month', created_at) = #{month}
+    SQL
+    record = HotWater.where(sql)
+
+    errors.add(:base, 'Значение может быть введено только раз в месяц') if record.exists?
+  end
 end
